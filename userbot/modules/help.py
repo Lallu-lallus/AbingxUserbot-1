b32c2a75b50bca6a98ba0fd6888294cf329d3f8c
Abing <97945597+nabilrmdaan@users.noreply.github.com>
2022-01-25 21:45:30 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -33,7 +33,7 @@ async def help(event):
         string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
-            string += "`\t 🏀  "
+            string += "`\t 𑁍  "
         await event.edit("**🥶 ABING COMMANDS**\n\n"
                          f"**◉ Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◉ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
                          "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
